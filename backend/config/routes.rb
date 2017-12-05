d57d80987900babf1b320f5268c75bbc076ab5fd
James Wang <jameswang97@ucla.edu>
2017-12-05 10:36:45 -0800

    Wrote backend functions, hope they work

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get '/grids/lookup/:locationName', to: 'grids#lookup'
+  put '/grids/colorupdate/:color', to: 'grids#colorupdate'
   resources :grids
   # add another route here for 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
